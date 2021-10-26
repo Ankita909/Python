8e8eda3eeafc1fc63da1e48781b6a61670b198b6
Ankita Kshatriya <58108015+Ankita909@users.noreply.github.com>
2021-10-26 19:18:27 +0530

    Data types in python
    type function

---
+++ data_types.py
@@ -0,0 +1,23 @@
+#data types
+
+marks=99.5
+name="Ankita"
+age=12
+
+is_correct= True
+the_complex_number=4+9j
+games_list=['ludo','snake and ladder','carrom','chess']
+
+print(marks,name,age)
+print(is_correct,the_complex_number,games_list)
+
+
+# printing its data type
+
+print(marks, type(marks))
+print(name, type(name))
+print (age, type(age))
+
+print(is_correct,type(is_correct))
+print(the_complex_number,type(the_complex_number))
+print(games_list,type(games_list))
