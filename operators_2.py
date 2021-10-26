dfec0363ee952ab2b1b65c0014749df06604f4cd
Ankita Kshatriya <58108015+Ankita909@users.noreply.github.com>
2021-10-26 20:00:54 +0530

    Operators in Python
    Relational and Logical operators

---
+++ operators_2.py
@@ -0,0 +1,34 @@
+# Operators in python
+
+#Relational/Comparison Operators : returns either true or false
+
+first_no=20
+sec_no=40
+
+print('using realational operators :')
+
+print(first_no > sec_no) 
+print(first_no < sec_no)
+print(first_no == sec_no)   #is first no. equal to the second number
+print (first_no != sec_no)  #not equal to
+print(first_no >= sec_no)  
+print(first_no <= sec_no)
+print('-------')
+#for equals to
+a=3
+b=3
+
+print(a==b)
+
+print('----------------------------------------------')
+
+#Logical Operators : must know the truth tables
+print('using Logical Operators')
+
+x=True
+y=False
+
+print(x and y)
+print(x or y)
+print(not x)
+print(not y)
