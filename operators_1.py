7a07d988df8adde3c614312d3b9eb9bc06479d7d
Ankita Kshatriya <58108015+Ankita909@users.noreply.github.com>
2021-10-26 19:48:47 +0530

    Python Operators
    Arithmetic and Assignment operators.

---
+++ operators_1.py
@@ -0,0 +1,33 @@
+# Operators in python
+
+
+#Arithmetic Operators
+
+#taking values in variables
+a=5
+b=2
+
+
+print('using Arithmetic Operators :')
+print(a+b)  #add
+print(a-b)  #subtract
+print(a*b)  #multiply
+print(a/b)  #divide
+
+print(a%b)  #modulo:gives the remainder
+print(a//b)  #quotient
+print(a**b)  #a to the power b
+
+print('-------------------------------------------------------')
+
+#Assignment Operators
+
+print('using Assignment Operators :')
+a+=5  # a= a+5
+print(a)
+b-=2  #b= b-2
+print(b)
+a*=5 #a=a*5  (10*5)
+print(a)
+a/=2  #a=a/2 (50/2)
+print(a)
