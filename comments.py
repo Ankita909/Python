596cdc7508495114e200bffa89ccd0a27a3c23cb
Ankita Kshatriya <58108015+Ankita909@users.noreply.github.com>
2021-10-26 19:35:34 +0530

    Comments in Python
    Single line and multi line comments

---
+++ comments.py
@@ -0,0 +1,12 @@
+# comments
+
+print('Comments in python are given using #')
+print('They are used for the better understanding of code ,that is, for documentationn purpose')
+print('Python ignores anything present after comment')
+
+# use triple single quotes for multi line comments
+
+''' hello
+how are you doing
+all good!
+'''
