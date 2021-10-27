60d71ef2d1a491a0333fadba2be0effd707f022d
Ankita Kshatriya <58108015+Ankita909@users.noreply.github.com>
2021-10-27 19:18:14 +0530

    if_condition
    Take age as user input and display whether the person can vote or not.

---
+++ condition_1.py
@@ -0,0 +1,8 @@
+print('Enter age:')
+age=int(input())
+
+if age > 18:   
+    print('You can Vote')   #this will print if the condition is satisfied that is True
+    
+    '''if the condition is not satisfied(false) ..Nothing will get printed'''
+    
