de809ce43e8eca82a7d8a15f391df23bbf0244df
Ankita Kshatriya <58108015+Ankita909@users.noreply.github.com>
2021-10-27 19:08:51 +0530

    Input_marks
    Input marks of three subjects Eng , Maths and Science and print their avg.

---
+++ input_marks.py
@@ -0,0 +1,13 @@
+
+#taking input as integers
+print('Enter English marks:')
+english= int(input())
+print('Enter Maths marks:')
+maths= int(input())
+print('Enter Science marks:')
+science= int(input())
+
+print("avg marks are:")
+avg=(english+maths+science)/3
+print(avg)
+
