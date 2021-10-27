0362d8b85314229adae596a043bdc73c21122d90
Ankita Kshatriya <58108015+Ankita909@users.noreply.github.com>
2021-10-27 21:41:13 +0530

    for_loop
    basic

---
+++ Loops.py
@@ -0,0 +1,7 @@
+# Python For Loop : the for loop in python is used to iterate over a sequence(list,tuple string) or other iterable objects.
+
+# iterating over a sequence is called traversal
+
+for x in range(10):
+    print(2*x)
+    print(2*x, end=" , ")   # gets printed in new line
