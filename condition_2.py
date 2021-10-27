439e9d8a76041e833a33664da90949568b21984a
Ankita Kshatriya <58108015+Ankita909@users.noreply.github.com>
2021-10-27 19:24:03 +0530

    if-else condition
    Check if the age entered by the person makes him eligible to vote or not.

---
+++ condition_2.py
@@ -0,0 +1,8 @@
+print('Enter age:')
+age=int(input())
+
+if (age > 18):   
+    print('You can Vote')   #this will print if the condition is satisfied that is True
+else:
+    print('You cannot Vote')
+    
