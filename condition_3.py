705b021588b5de98216154bb532d68f092f47185
Ankita Kshatriya <58108015+Ankita909@users.noreply.github.com>
2021-10-27 19:30:42 +0530

    if-el-if condition
    Check whether the number entered by the user is positive , negative or zero.

---
+++ condition_3.py
@@ -0,0 +1,12 @@
+#check whether the number is positive , negative or zero
+
+print('Enter any number:')
+num=float(input())
+
+if (num >0):   
+    print('Positive Number')  
+elif(num==0):
+    print('Zero')
+else:
+    print('Negative Number')
+    
