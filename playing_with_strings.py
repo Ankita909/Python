8b9a8adfc07cb59b4ece8bab9c83d3f7f0620305
Ankita Kshatriya <58108015+Ankita909@users.noreply.github.com>
2021-11-04 10:42:00 +0530

    Concat and repeat
    Adding two strings 
    Repeating a string itself

---
+++ playing_with_strings.py
@@ -0,0 +1,12 @@
+# concatenation
+
+a="Ank"
+b='ita'
+c=a+b #concatenation operator: '+'
+print(c)
+
+#repeating
+c=a*2 # string a will repeat itself twice
+print(c)
+c=b*4 # string b will repeat itself four times
+print(c)
