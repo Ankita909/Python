d655f4c16dac8b2dd53bb803244e80e91e54f12f
Ankita Kshatriya <58108015+Ankita909@users.noreply.github.com>
2021-11-04 10:45:13 +0530

    Strings in Python for loop
    for loop in strings

---
+++ Strings_in_python_for_loops.py
@@ -0,0 +1,13 @@
+# for loops  with Strings
+
+a="Ankita"
+
+for my_char in a:
+    print(my_char)
+
+b="Kshatriya"
+ 
+ #-------------------------------------------------------------
+
+for the_char in b:
+    print(the_char*3)
