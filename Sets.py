bbbd0d05fbd91558b4098d5b9ae1aa9a6746bdbd
Ankita Kshatriya <58108015+Ankita909@users.noreply.github.com>
2021-11-05 12:05:29 +0530

    Sets

---
+++ Sets.py
@@ -0,0 +1,12 @@
+#----------------------Sets------------------------
+
+''' elements are not repeated 
+elements cannot be accessed using index : elements are in random fashion'''
+
+a={1,2,3,4,3,5,6,7,3,2,1,3}
+print(a)  #pops out repeated elements
+
+# iteration
+
+for element in a:
+    print(element*2)
