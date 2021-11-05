6d4fe2c9202c655a21d1eb5e21a60505b1332363
Ankita Kshatriya <58108015+Ankita909@users.noreply.github.com>
2021-11-05 11:50:29 +0530

    Tuples

---
+++ Tuples.py
@@ -0,0 +1,29 @@
+#----------------------Tuples in python------------------------
+
+''' Like in strings we can not alter its elements(characters) , similarly in tuples also we cannot alter the elements . That is they are immutable'''
+ 
+''' s="Ankita"
+ s[0]='v'
+ print(s)  #error 
+ 
+'''
+b=['A','B','C']
+b[0]='v'
+print(b)  #works
+
+''' Tuples  are immutable.:elements if a tuple cannot bne changed once it has been assigned
+ tuple is created by placing all the items inside parenthesis(),seperated by commas.
+ The parenthesis are optional, however, it is a good practie to use them'''
+
+c=('a','b','c','d')
+print(c)
+  
+
+  #functions will be same as lists
+
+
+  
+ 
+ 
+
+
