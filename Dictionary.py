4628ee3e44fd150ff19d4865917914f30f6d29ff
Ankita Kshatriya <58108015+Ankita909@users.noreply.github.com>
2021-11-05 12:10:58 +0530

    Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,13 @@
+#------------------------Dictionary------------------------
+
+'''Python dictionary is an unordered collection of items.
+While other compound data types have only value as an element , a dictionary has a key :vakue pair.'''
+
+
+marks={'Ankita': 80, 'Akash': 90, 'Srishti': 80}
+print(marks)
+
+print(marks['Ankita']) #accessing value using key
+
+marks2={'Ankita': 80, 'Akash': 90, 'Srishti': 80, 20:100}
+print(marks2[20]) #accessing value using key
