e72f21ba9abd676cc20eba45f43926470e1acae3
Ankita Kshatriya <58108015+Ankita909@users.noreply.github.com>
2021-11-05 11:21:10 +0530

    List Functions

---
+++ List_functions.py
@@ -0,0 +1,12 @@
+#--------------------------List Functions-------------------------
+
+a=[1,9,7,6,5,6,5,4,3,2]
+
+print(len(a))  #length of the list
+print(max(a))  #highest element
+print(min(a))  #lowest element
+print(sum(a))  #sums up all the elements
+
+
+s="Ankita Kshatriya"
+print(list(s)) #seperates all the characters
