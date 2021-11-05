e2439e26bec0fe226876b9459d821cb5e7e6f4d6
Ankita Kshatriya <58108015+Ankita909@users.noreply.github.com>
2021-11-05 11:55:20 +0530

    Tuple functions

---
+++ Tuple_functions.py
@@ -0,0 +1,16 @@
+#------------------------Tuple Operations------------------------------
+
+
+''' Accessing element from a tuple:
+
+1. Positive and negative indexing --> like Lists
+2. Slicing --> like Lists
+3. Iterating through a tuple --> like lists   (for loop)
+
+Tuple functions:
+
+len(tuple)
+max(tuple)
+tuple(seq)
+min(tuple)
+sum(tuple) '''
