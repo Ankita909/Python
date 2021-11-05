2edab6e3131a6e618d8e6d050cd46962a09bd205
Ankita Kshatriya <58108015+Ankita909@users.noreply.github.com>
2021-11-05 12:21:57 +0530

    Updating dictionary

---
+++ Updating_dictionary.py
@@ -0,0 +1,13 @@
+#--------------------Updating Dictionary items-------------------------
+''' can also update a dictionary by modifying existing a key-value pair or by merging another dictionary to an existing one.
+
+can add new items or change the value of existing items using assignment operator.
+
+if the key is already present the value gets updated,else a new key:value pair ios added to the dictionary'''
+
+marks={'Ankita': 80, 'Akash': 90, 'Srishti': 80, 20:100}
+
+marks['Anjali']=99
+marks['Ankita']=30
+
+print(marks)
