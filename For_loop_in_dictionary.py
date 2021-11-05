5fb798bfd1671194064aea334f90b1ec22fc202c
Ankita Kshatriya <58108015+Ankita909@users.noreply.github.com>
2021-11-05 12:14:53 +0530

    Iterating through a dictionary

---
+++ For_loop_in_dictionary.py
@@ -0,0 +1,8 @@
+#------------------for_loop in Dictionary----------------
+
+'''Iterating through a dictionary:
+Using a for loop we can iterate through each key in a dictionary'''
+
+squares ={1:1, 2:4, 3:9, 4:16, 5:25, 6:36}
+for i in squares :
+    print(i,squares[i])
