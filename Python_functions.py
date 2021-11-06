16074e2ae156571d68f3ac35f2f16739658e2c63
Ankita Kshatriya <58108015+Ankita909@users.noreply.github.com>
2021-11-06 10:25:54 +0530

    Functions_in_Python

---
+++ Python_functions.py
@@ -0,0 +1,19 @@
+#-----------------------Functions in Python--------------------------
+
+'''Function in python is a group of related statements that perform a specific task. 
+
+Functions help break our program into smaller and modular chunks .As our program grows larger and larger , functions make it more organized and manageble'''
+
+'''
+ Categories of Functions:
+1. Built-in: int() float() input() eval() len() type() min()/max() abs() round() range()
+
+2. Modules:
+        A file containing functions and variables defined in seperate files.
+        A module is simply a file that contains Python Code.
+        When we break a program into modules, each module should contain functions that perform related tasks.
+    math()
+    random()
+    string()
+3. User Defined
+'''
