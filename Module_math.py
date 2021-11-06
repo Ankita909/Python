e3ddcac9547bdb07ba3701388b83144d2b97383f
Ankita Kshatriya <58108015+Ankita909@users.noreply.github.com>
2021-11-06 10:26:18 +0530

    Math_module

---
+++ Module_math.py
@@ -0,0 +1,11 @@
+#-------------------Modules in python--------------------------
+
+import math as m # if the module name is bit lengthy then you can give it a name of your choice by adding 'as'.(this is called alias)
+# if you need any one or two functions only then import as follows:
+# from math import cos,sin,pi
+# from math import*   (to import all the functions of maath module)
+#dir(math) # shows all the functions in math module
+
+print(m.pi)
+print(m.cos(90))
+print(m.sin(m.pi/2))
