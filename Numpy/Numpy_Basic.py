62b20e5250868b06c82f11307ed418ad29a1df1f
Ankita Kshatriya <58108015+Ankita909@users.noreply.github.com>
2021-12-24 19:47:45 +0530

    array()
    Array function

---
+++ Numpy/Numpy_Basic.py
@@ -0,0 +1,26 @@
+# ----------creating and printing 1-D array---------------
+import numpy as np
+a=np.array([2,3,4,5])
+print(a)
+
+#-----------creating and printing 2-D array--------------
+b=np.array([[2,3,4,5],[6,6,7,8]])
+print(b)
+
+#-----------------------upcasting----------------------
+c=np.array([1,2,3.0])
+print(c)
+
+#-----------------------minimum dimension---------------
+d=np.array([2,3,4],ndmin=2)
+print(d)
+e=np.array([2,3,4],ndmin=3)
+print(e)
+f=np.array([2,3,4],ndmin=1)
+print(f)
+
+#-----------Changing the data type of the array------
+g=np.array([2,8,16],dtype="complex")
+print(g)
+h=np.array([(1,2),(3,5)],dtype=[('a','<i4'),('b','<i4')])
+print(h['a'])
