6ca503b304484a2485803fbb81e945951cb2b859
Ankita Kshatriya <58108015+Ankita909@users.noreply.github.com>
2021-12-25 10:52:30 +0530

    Random Module
    randn
    randint
    ranf
    rand

---
+++ Numpy/Numpy_Basic8.py
@@ -0,0 +1,18 @@
+import numpy as np
+#help(np.random)
+#help(np.random.randn)
+#help(np.random.randint)
+#help(np.random.ranf)
+
+a=np.random.randn(4)
+print(a)
+b=np.random.rand(3,4)
+print(b)
+c=np.random.ranf(3)
+print(c)
+d=np.random.randint(2,size=20)
+print(d)
+e=np.random.randint(1,size=20)
+print(e)
+f=np.random.randint(5,size=20)
+print(f)
